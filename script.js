3016fbc94b9c3d62cb3deb6cc5602c2978391e54
aleksandrayakovleva132 <aleksandrayakovleva132@gmail.com>
2022-06-15 17:43:05 +0300

    start to make animation

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log('Скрипт подключен');
+
+const animItems = document.querySelectorAll('.anim-item');
+
+if(animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll)
+  function animOnScroll(params) {
+      for( let index = 0; index < animItems.length; index++) {
+          const animItem = animItems[index];
+          const animItemHeight = animItem.offsetHeight;
+          const animaItemOffset = offset(animItem).top;
+          const animStart = 1;
+
+          let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+          if (animItemHeight > window.innerHeight) {
+              animItemPoint = window.innerHeight - animItemHeight / animStart;
+          }
+
+          if((pageYOffset > animaItemOffset - animItemPoint) && animaItemOffset < (animaItemOffset + animItemHeight)) {
+              animItem.classList.add('active');
+          } else {
+              animItem.classList.remove('active');
+          }
+      }
+  }
+  function offset(el) {
+      const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop =  window.pageYOffset || document.documentElement.scrollTop;
+
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  }
+}
